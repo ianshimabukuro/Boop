a4907eebf4fe495fbc4094ab2b5442fd1ce7d521
Ian Shimabukuro <shimakun000@gmail.com>
2025-05-26 20:04:56 -0700

    init commit

--- Boop Watch App/ContentView.swift
+++ Boop Watch App/ContentView.swift
@@ -8,16 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var isDead : Bool = false
+    @State var currentTime: Int = 3600
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+   
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Time Left: \(currentTime)")
+                .fontWeight(.bold)
+            if isDead{
+                Image("DeadDog")
+                    .resizable()
+                    .frame(width: 120,height: 120)
+                Text("He DED :((((((")}
+            else{
+                Image("AliveDog")
+                    .resizable()
+                    .frame(width: 120,height: 120)
+                Button("Feed Me"){
+                    currentTime = 3600
+                }
+                .font(.system(size: 30)) // Explicitly use a small font size
+                .padding(.vertical, 4)   // Minimal vertical padding
+                .padding(.horizontal, 8) // Minimal horizontal padding
+                .background(Color.brown)
+                .foregroundColor(.white)
+                .cornerRadius(6)
+                .buttonStyle(PlainButtonStyle()) // Removes default button styling
+                .minimumScaleFactor(0.5)  // Makes the text smaller
+            }
+
+        }.onReceive(timer){
+            _ in
+            if currentTime>0 {
+                currentTime -= 1
+            }
+            if currentTime == 0{
+                isDead = true
+            }
+        
+
         }
-        .padding()
     }
 }
+
 
 #Preview {
     ContentView()
