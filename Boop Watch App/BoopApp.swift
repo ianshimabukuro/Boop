b8cc27882b7a6b5b0032895347f47fe1c09a9960
Ian Shimabukuro <shimakun000@gmail.com>
2025-05-26 18:59:44 -0700

    Initial Commit

---
+++ Boop Watch App/BoopApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoopApp.swift
+//  Boop Watch App
+//
+//  Created by Ian Shimabukuro on 2025/05/26.
+//
+
+import SwiftUI
+
+@main
+struct Boop_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Boop Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Boop Watch App
+//
+//  Created by Ian Shimabukuro on 2025/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
